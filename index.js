35ceafcee51d3a5c0074bd3860bb9bd1a6cc9274
cheoljin <chur7072@naver.com>
2022-12-20 19:24:39 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// application/json
+app.use(express.json());
 
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    "mongodb+srv://cheoljin:qetgda13579!@boilerplate.ss65pvb.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log("MongoDB is connected!!");
   })
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.send("Hello World! 새해 복 많이 받으세요! 앙 개꿀띠")
+);
+
+app.post("/register", (req, res) => {
+  // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`Example app Listening on port ${port}!`));
